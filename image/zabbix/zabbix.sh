e4179a0560e0c5467a7f70531beed9bc14d05937
Michael Kheyfets <1675480@gmail.com>
2015-03-26 14:24:19 +0100

    Update zabbix.sh

--- image/zabbix/zabbix.sh
+++ image/zabbix/zabbix.sh
@@ -3,8 +3,10 @@ set -e
 source /build/buildconfig
 set -x
 
-curl -O http://repo.zabbix.com/zabbix/2.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.4-1+trusty_all.deb
-dpkg -i zabbix-release_2.4-1+trusty_all.deb
+#curl -O http://repo.zabbix.com/zabbix/2.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.4-1+trusty_all.deb
+#dpkg -i zabbix-release_2.4-1+trusty_all.deb
+curl -O http://repo.zabbix.com/zabbix/2.4/ubuntu/pool/main/z/zabbix/zabbix-frontend-php_2.4.4-1+trusty_all.deb
+dpkg -i zabbix-frontend-php_2.4.4-1+trusty_all.deb
 apt-get update
 $minimal_apt_get_install zabbix-frontend-php php5-mysql php5-pgsql
 
